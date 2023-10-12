b0a1ae0ea8d3bdcafbb4e69c5972a441a3f62a47
Ali Cagatay <aliccagatay@gmail.com>
2023-10-12 14:14:53 +0100

    Update `index.js`.

--- index.js
+++ index.js
@@ -1,12 +1,21 @@
-const divs = document.querySelectorAll("div");
-
+const div1 = document.getElementsByClassName("inner-inner-box1");
+const div2 = document.getElementsByClassName("inner-inner-box2");
+const div3 = document.getElementsByClassName("inner-box");
+const div4 = document.getElementsByClassName("outer-box");
 const body = document.querySelector("body");
 
-function logText(e) {
+function logText1(e) {
   console.log(this.classList.value);
   //e.stopPropagation();
 }
 
-divs.forEach((div) => div.addEventListener("click", logText));
+function logText2(e) {
+  console.log(this.classList.value);
+  e.stopPropagation();
+}
 
-body.addEventListener("click", logText);
+div1[0].addEventListener("click", logText1);
+div2[0].addEventListener("click", logText2);
+div3[0].addEventListener("click", logText1);
+div4[0].addEventListener("click", logText1);
+body.addEventListener("click", logText1);
