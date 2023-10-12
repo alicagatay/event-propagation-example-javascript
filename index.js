4021c618d29fae972ef4bfe76d4947faa4e45699
Ali Cagatay <aliccagatay@gmail.com>
2023-10-12 13:20:45 +0100

    Added event listeners for both the `div` and the `body` components.

--- index.js
+++ index.js
@@ -5,3 +5,7 @@ const body = document.querySelector("body");
 function logText(e) {
   console.log(this.classList.value);
 }
+
+divs.forEach((div) => div.addEventListener("click", logText));
+
+body.addEventListener("click", logText);
